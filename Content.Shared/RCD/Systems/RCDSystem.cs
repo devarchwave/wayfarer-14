8acc3de0292f7944da450838f5838bf2e058866f
JesterX666 <32009105+JesterX666@users.noreply.github.com>
2025-11-04 22:23:43 -0500

    Log radial menu interractions for the RCD, Issue #40902 (#40986)
    * Log radial menu interractions,  Issue #40902
    
    * code review adjustement
    
    * Asked modifications from code review
    
    * Update Content.Shared/RCD/Systems/RCDSystem.cs
    
    Yeah, I'm old and using a very old C# syntax :D  I need to update my brain
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>
    
    * Update Content.Shared/RCD/Systems/RCDSystem.cs
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: slarticodefast <161409025+slarticodefast@users.noreply.github.com>

--- Content.Shared/RCD/Systems/RCDSystem.cs
+++ Content.Shared/RCD/Systems/RCDSystem.cs
@@ -1,6 +1,5 @@
 using Content.Shared.Access.Components;
 using Content.Shared.Administration.Logs;
-using Content.Shared.Charges.Components;
 using Content.Shared.Charges.Systems;
 using Content.Shared.Construction;
 using Content.Shared.Database;
@@ -102,11 +101,14 @@ public sealed class RCDSystem : EntitySystem
         if (!component.AvailablePrototypes.Contains(args.ProtoId))
             return;
 
-        if (!_protoManager.HasIndex(args.ProtoId))
+        if (!_protoManager.Resolve<RCDPrototype>(args.ProtoId, out var prototype))
             return;
 
         // Set the current RCD prototype to the one supplied
         component.ProtoId = args.ProtoId;
+
+        _adminLogger.Add(LogType.RCD, LogImpact.Low, $"{args.Actor} set RCD mode to: {prototype.Mode} : {prototype.Prototype}");
+
         Dirty(uid, component);
     }
 
