cbb35a0a768ce8b4ee59dd2ce0918f4816fbfbd1
DevArchwave <168038123+DevArchwave@users.noreply.github.com>
2026-05-20 20:38:17 -0300

    Better desc

--- Content.Shared/_WF/Damage/DamageCapComponent.cs
+++ Content.Shared/_WF/Damage/DamageCapComponent.cs
@@ -6,7 +6,7 @@ namespace Content.Shared._WF.Damage;
 public sealed partial class DamageCapComponent : Component
 {
     /// <summary>
-    /// Maximum allowed damage for all damage types. (Yes, this is generalized. Fight me.)
+    /// Maximum allowed damage for all damage types. (Yes, this is generalized. Fight me.) A cap of 3000 means you can have, for example, 3000 burn, 3000 brute, 3000 slash, so 3000 in any type.
     /// </summary>
     [DataField]
     public int DamageCap = 3500;
