18356095d1de90651f55cae8400bf8a17e871c00
DevArchwave <168038123+DevArchwave@users.noreply.github.com>
2026-06-27 02:27:45 -0300

    Disables internal anomalies from gibbing players on supercrit

--- Content.Server/Anomaly/Effects/InnerBodyAnomalySystem.cs
+++ Content.Server/Anomaly/Effects/InnerBodyAnomalySystem.cs
@@ -134,7 +134,7 @@ public sealed class InnerBodyAnomalySystem : SharedInnerBodyAnomalySystem
         if (!TryComp<BodyComponent>(ent, out var body))
             return;
 
-        _body.GibBody(ent, true, body, splatModifier: 5f);
+        //_body.GibBody(ent, true, body, splatModifier: 5f); // Wayfarer
     }
 
     private void OnSeverityChanged(Entity<InnerBodyAnomalyComponent> ent, ref AnomalySeverityChangedEvent args)
